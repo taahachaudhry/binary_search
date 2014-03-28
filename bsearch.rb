23d4660f091cb661718b7bb422bb7c837a2b9325
Taaha Chaudhry <t44h413@gmail.com>
2014-03-28 13:19:47 -0500

    Binary search completed with tests.

--- bsearch.rb
+++ bsearch.rb
@@ -4,6 +4,11 @@ module ArrayUtil
   def self.bsearch(array, element)
     # If the array is empty return a result
     # Otherwise call your helper _bsearch method
+    if array.length==0
+      false
+    else
+      _bsearch(array,element,0,array.size-1)
+    end
   end
 
   def self._bsearch(array, element, start_index, end_index)
@@ -16,5 +21,20 @@ module ArrayUtil
     # If the element is greater than the one at midpoint, call
     #   _bsearch on the right half of the remaining array
     # Otherwise, call _bsearch on left half of the remaining array
+    if array[start_index] == array[end_index] && array[start_index] != element
+      false
+    elsif array[start_index] == array[end_index] && array[start_index] == element
+      true
+    else
+      midpoint = start_index + (end_index - start_index) / 2
+      #binding.pry
+      if element <= array[midpoint]
+        end_index=end_index=midpoint
+        _bsearch(array,element,start_index,end_index)
+      else
+        start_index=midpoint+1
+        _bsearch(array,element,start_index,end_index)
+      end
+    end
   end
 end
